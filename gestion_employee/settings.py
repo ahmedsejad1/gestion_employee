d5d617b6bbd3547614d4b5796f9fc533b4db2cd0
ahmedsejad1 <sejadahmedinfo@gmail.com>
2022-12-11 13:04:12 +0000

    remove secret key

--- gestion_employee/settings.py
+++ gestion_employee/settings.py
@@ -19,8 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-(nxub3g&sgm20^6++1(-x1u&+!7zg6squ(al@w1m=p!od9^2r^'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
